a5473ddffb072ec6e6ad1bb459c9d2a16ce4f8c2
caffeinatedbyte <60981503+caffeinatedbyte@users.noreply.github.com>
2025-12-06 09:52:38 +0530

    Rename width and height inputs for clarity

--- home.py
+++ home.py
@@ -11,8 +11,8 @@ st.title("Automobile data case study")
 symbolizing=[-2,-1,0,1,2,3]
 sym=st.selectbox('Symbolic',symbolizing,index=symbolizing.index(-2))
 nl=st.number_input('Normalised losses',value=0,step=1)
-w=st.number_input('Width (mm)',format='%d',value=0,step=1)   
-h=st.number_input('Height (mm)',format='%d',value=0,step=1)
+w=st.number_input('Width',format='%d',value=0,step=1)   
+h=st.number_input('Height',format='%d',value=0,step=1)
 es=st.number_input('Engine Size (cc)',format='%d',value=0,step=1)#integer
 hp=st.number_input('Horse power (bhp)',format='%d',value=0,step=1)#integer
 cm=st.number_input('City Mileage (km/l)',format='%d',value=0,step=1)
